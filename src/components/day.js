9941822e07fcb96981017c2a7a2e08951d47d5a3
voiceinthedark <darkrisingforce@gmail.com>
2021-12-16 00:13:39 +0200

    - More visual tweaking

--- src/components/day.js
+++ src/components/day.js
@@ -1,22 +1,39 @@
-import snowy from '../images/snowy.png';
-import cloudy from '../images/cloudy.png'
+import snow from '../images/snowy.png';
+import cloud from '../images/cloudy.png'
+import rain from '../images/rainy.png'
 import low from '../images/low.svg'
 import precipitation from '../images/precipitation.svg'
 import React from 'react';
 import './day.css';
 
 const Day = ({day}) => {
-    // console.log(day);
+    const weatherToday = day.weather[0]['main'].toLowerCase();
+    let imgSrc = '';
+    switch (weatherToday) {
+      case 'rain':
+        imgSrc = rain;        
+        break;
+        case 'snow':
+          imgSrc = snow;
+          break;
+          case 'cloud':
+            imgSrc = cloud;
+            break;    
+      default:
+        break;
+    }
+
     return (
-      <div className={'day ' + (day.weather[0]['main']).toLowerCase()}>
-        <img src={snowy} className='icon' height={'75px'} width={'120px'} />
-        <div className='temperature'>{day.temp['day']}</div>
+      <div className={'day ' + weatherToday}>
+        <img src={imgSrc} className='icon' height={'120px'} width={'120px'} />
+        <div className='temperature'>{Math.round(day.temp['day']) + '°'}</div>
         <div className='content'>
           <div>
-            <img src={precipitation} /> {day.pop}
+            <img src={precipitation} className='precipitation' />{' '}
+            {day.pop * 100 + '%'}
           </div>
           <div>
-            <img src={low} /> {day.temp['min']}
+            <img src={low} className='low' /> {Math.round(day.temp['min'])}
           </div>
         </div>
       </div>
