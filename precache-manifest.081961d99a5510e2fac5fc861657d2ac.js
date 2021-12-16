89d709077702f792f2dddafb535cb1813764a9dc
voiceinthedark <darkrisingforce@gmail.com>
2021-12-17 00:46:30 +0200

    Updates

---
+++ precache-manifest.081961d99a5510e2fac5fc861657d2ac.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "679ebedbbf6bf3bd01fb",
+    "url": "/React-Weather/static/css/main.7fd2ec36.chunk.css"
+  },
+  {
+    "revision": "679ebedbbf6bf3bd01fb",
+    "url": "/React-Weather/static/js/main.679ebedb.chunk.js"
+  },
+  {
+    "revision": "e5fda52e5de68f25510f",
+    "url": "/React-Weather/static/js/1.e5fda52e.chunk.js"
+  },
+  {
+    "revision": "2954d3a088fa604e0f55",
+    "url": "/React-Weather/static/js/runtime~main.2954d3a0.js"
+  },
+  {
+    "revision": "68e7ab854595e1354b247878e2aa614b",
+    "url": "/React-Weather/static/media/snowy.68e7ab85.png"
+  },
+  {
+    "revision": "c365103687af6f29c71797ec41be794f",
+    "url": "/React-Weather/static/media/cloudy.c3651036.png"
+  },
+  {
+    "revision": "5ddda34fcc68ae015a09bb324b19a540",
+    "url": "/React-Weather/static/media/rainy.5ddda34f.png"
+  },
+  {
+    "revision": "a049997e42546ac2159751fd669abc60",
+    "url": "/React-Weather/static/media/drizzle.a049997e.png"
+  },
+  {
+    "revision": "bc801d08bb2ca972e7f1bb8a068b379c",
+    "url": "/React-Weather/static/media/sunny.bc801d08.png"
+  },
+  {
+    "revision": "0fd3e012552426149cbe8f670ac7f9e2",
+    "url": "/React-Weather/static/media/thunderstorm.0fd3e012.png"
+  },
+  {
+    "revision": "902509ddbaa9ffad106c60ed4ab7407c",
+    "url": "/React-Weather/static/media/weather.902509dd.png"
+  },
+  {
+    "revision": "2e9adf90f1df94315eae68b1c025ea04",
+    "url": "/React-Weather/static/media/low.2e9adf90.svg"
+  },
+  {
+    "revision": "0ed83d8815c19e102259dbe4c7242241",
+    "url": "/React-Weather/static/media/precipitation.0ed83d88.svg"
+  },
+  {
+    "revision": "099c768afd18348e84412b121af69679",
+    "url": "/React-Weather/index.html"
+  }
+];
